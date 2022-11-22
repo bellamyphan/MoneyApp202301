a8bcebfef3be01c7237ea4a75b76410f7363849d
bellamyphan <bellamyphan@icloud.com>
2022-11-22 12:37:04 -0600

    Add header line to WriterDao classes

--- src/main/java/dao/transaction/TransactionWriterDAO.java
+++ src/main/java/dao/transaction/TransactionWriterDAO.java
@@ -1,4 +1,10 @@
 package dao.transaction;
 
 public class TransactionWriterDAO {
+    String[] headerLine;
+
+    public TransactionWriterDAO() {
+        headerLine = new String[]{"Id", "Transaction Type", "Date", "Amount", "Type", "Note", "Name", "Location",
+                "Primary Bank", "Secondary Bank", "Is Pending"};
+    }
 }

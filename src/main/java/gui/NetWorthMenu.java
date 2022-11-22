d2d3ed977d03936a2a4b6c7e71866d93e3808f69
bellamyphan <bellamyphan@icloud.com>
2022-11-22 15:35:54 -0600

    Add a new Net Worth Menu

---
+++ src/main/java/gui/NetWorthMenu.java
@@ -0,0 +1,11 @@
+package gui;
+
+public class NetWorthMenu extends BasicMenu {
+    // Calculate my net worth as of today.
+    // Calculate my net worth as of a date in the past.
+    // Statistic my network in the last 12 months.
+    // Statistic my network in the last 5 years.
+    // Statistic my network in the last 10 years.
+    // Statistic my network in the last 20 years.
+    // Statistic my network in the last x years.
+}

4ee97c2d2f0d9a8861f6c3226557f048a804e4e8
bellamyphan <bellamyphan@icloud.com>
2022-11-22 18:48:49 -0600

    Filter transactions based on Start and End Dates

--- src/main/java/development/TypeReportDevelopment.java
+++ src/main/java/development/TypeReportDevelopment.java
@@ -1,17 +1,24 @@
 package development;
 
 import dao.transaction.TransactionReaderDAO;
+import objects.transaction.Transaction;
 import objects.transaction.TransactionHandler;
+import tools.DateHandler;
+
+import java.util.Date;
+import java.util.List;
 
 public class TypeReportDevelopment {
     public static void main(String[] args) {
+        List<Transaction> allTransactions = new TransactionReaderDAO().getTransactions();
+
+        Date startDate = new DateHandler("2018-10-10").getDate();
+        Date endDate = new DateHandler("2018-10-15").getDate();
+        List<Transaction> filteredTransactions = new TransactionHandler(allTransactions)
+                .getFilteredTransactions(startDate, endDate);
 
 
-        System.out.println(new TransactionHandler(new TransactionReaderDAO().getTransactions()));
-
-
-
-
+        System.out.println(new TransactionHandler(filteredTransactions));
 
     }
 }

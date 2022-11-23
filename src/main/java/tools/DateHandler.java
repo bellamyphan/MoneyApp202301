cd8bf676ccdf198ae2b63f55e3032c740cc3e00c
bellamyphan <bellamyphan@icloud.com>
2022-11-22 18:15:24 -0600

    Update DateHandler

--- src/main/java/tools/DateHandler.java
+++ src/main/java/tools/DateHandler.java
@@ -2,36 +2,52 @@ package tools;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class DateHandler {
-    String yyyyMMddString;
+    Date date;
 
-    public DateHandler(String yyyyMMddString) {
-        this.yyyyMMddString = yyyyMMddString;
-    }
-
-    public DateHandler(Date date) {
+    public DateHandler(String dateString) {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        yyyyMMddString = simpleDateFormat.format(date);
-    }
-
-    public Date getDate() {
-        // If we have an empty date string.
-        if (yyyyMMddString.length() == 0) {
-            return null;
+        if (dateString.length() < 10) {
+            dateString += "-01";
         }
-        // If we have a valid string date format.
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
         try {
-            return simpleDateFormat.parse(yyyyMMddString);
+            date = simpleDateFormat.parse(dateString);
         } catch (ParseException e) {
             throw new RuntimeException(e);
         }
     }
 
+    public DateHandler(Date date) {
+        this.date = date;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public Date getMinDateOfMonth() {
+        String newDateString = getSimpleDateString().substring(0, 7);
+        return new DateHandler(newDateString).getDate();
+    }
+
+    public Date getMaxDateOfMonth() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        int maxDay = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+        String newDateString = getSimpleDateString().substring(0, 8) + maxDay;
+        return new DateHandler(newDateString).getDate();
+    }
+
     @Override
     public String toString() {
-        return yyyyMMddString;
+        return getSimpleDateString();
+    }
+
+    private String getSimpleDateString() {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        return simpleDateFormat.format(date);
     }
 }

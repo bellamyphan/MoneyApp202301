b06ce72f1aa8a7723c23dc45b289dfa46b32c185
bellamyphan <bellamyphan@icloud.com>
2022-11-22 19:10:45 -0600

    Type Report for filtered transactions by Date

--- src/main/java/development/TypeReportDevelopment.java
+++ src/main/java/development/TypeReportDevelopment.java
@@ -1,24 +1,14 @@
 package development;
 
-import dao.transaction.TransactionReaderDAO;
-import objects.transaction.Transaction;
-import objects.transaction.TransactionHandler;
+import objects.type.TypeReportHandler;
 import tools.DateHandler;
 
 import java.util.Date;
-import java.util.List;
 
 public class TypeReportDevelopment {
     public static void main(String[] args) {
-        List<Transaction> allTransactions = new TransactionReaderDAO().getTransactions();
-
-        Date startDate = new DateHandler("2018-10-10").getDate();
-        Date endDate = new DateHandler("2018-10-15").getDate();
-        List<Transaction> filteredTransactions = new TransactionHandler(allTransactions)
-                .getFilteredTransactions(startDate, endDate);
-
-
-        System.out.println(new TransactionHandler(filteredTransactions));
-
+        Date startDate = new DateHandler("2018-10-01").getDate();
+        Date endDate = new DateHandler("2018-10-31").getDate();
+        System.out.println(new TypeReportHandler().getTypeReportFilterByTime(startDate, endDate));
     }
 }
--- src/main/java/objects/type/TypeReportHandler.java
+++ src/main/java/objects/type/TypeReportHandler.java
@@ -1,4 +1,38 @@
 package objects.type;
 
+import dao.transaction.TransactionReaderDAO;
+import objects.amount.AmountObject;
+import objects.transaction.Transaction;
+import objects.transaction.TransactionHandler;
+import objects.transaction.TransactionObject;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
 public class TypeReportHandler {
+    public String getTypeReportFilterByTime(Date start, Date end) {
+        StringBuilder result = new StringBuilder();
+        AmountObject overallBalance = new AmountObject(new BigDecimal("0"));
+        List<Transaction> allTransactions = new TransactionReaderDAO().getTransactions();
+        List<Transaction> filteredTransactions = new TransactionHandler(allTransactions)
+                .getFilteredTransactions(start, end);
+        for (Type type : Type.values()) {
+            AmountObject balance = new AmountObject(new BigDecimal("0"));
+            for (Transaction transaction : filteredTransactions) {
+                if (transaction instanceof TransactionObject) {
+                    if (((TransactionObject) transaction).getType() == type) {
+                        balance = balance.add(transaction.getAmount());
+                    }
+                }
+            }
+            if (balance.getAmount().compareTo(new BigDecimal("0")) == 0) {
+                continue;
+            }
+            overallBalance = overallBalance.add(balance);
+            result.append(type.toString()).append(": ").append(balance).append("\n");
+        }
+        result.append("OVERALL BALANCE: ").append(overallBalance);
+        return result.toString();
+    }
 }
